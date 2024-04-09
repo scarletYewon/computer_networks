af81eacbccc5d619a69c9e63c9b0c5736dc46cc2
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-04-10 02:26:16 +0900

    에러 처리 완료

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -4,7 +4,7 @@ import select
 print("Student ID : 20213047")
 print("Name : Yewon Lee")
 
-while (1): 
+while (True):
   # 소켓생성
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   port = 80
@@ -24,8 +24,6 @@ while (1):
     
     serverName[2] = numSN
 
-    print(numSN)
-
     # 소켓연결
     try :
       serverIp = socket.gethostbyname(serverName[2])
@@ -38,7 +36,7 @@ while (1):
       continue
 
     if (serverName[0] != "http:"):
-      print("Only Support http, not", serverName[0][:-1],"\r\n")
+      print("Only support http, not", serverName[0][:-1],"\r\n")
       continue
 
     # 요청보내기
@@ -52,42 +50,63 @@ while (1):
 
   # 응답받기
   response = b''
+  cnt = 0
+  tmp = 0
   while select.select([s],[],[],3)[0]:
     d = s.recv(1024)
     if not d:
       break
-    response += d
-    # print(f"Current Downloading {sum}/{total} (bytes) {(sum/total)*100}%",end="\r")
+    # 전체값 찾기
+    response+=d
+    headers = response.split(b'\r\n\r\n')[0]
 
-
-
-  headers =  response.split(b'\r\n\r\n')[0]
-  image = response[len(headers)+4:]
-
-   # 전체 응답 크기 파악
-  total = None
-  for i in headers.split(b'\r\n'):
-    if i.startswith(b'Content-Length:'):
-      total = int(i.split(b':')[1])
+    # 상태코드에 따른 에러처리
+    st=0
+    sCode = headers.split(b'\r\n')[0].decode()
+    status = sCode.split()
+    if (int(status[1]) != 200):
+      print(" ".join(status[1:]),"\r\n\r\n")
+      st+=1
       break
-      
-  print("Total Size",total,"bytes")
 
-  # 상태코드에 따른 에러처리
-  sCode = headers.split(b'\r\n')[0].decode()
-  status = sCode.split()
-  if (int(status[1]) != 200):
-    print(" ".join(status[1:]),"\r\n")
+    # 전체값 찾기
+    if tmp < 1:
+      for i in headers.split(b'\r\n'):
+        if i.startswith(b'Content-Length:'):
+          total = int(i.split(b':')[1])
+          print("Total size",total,"bytes")
+          tmp+=1
+          break
+
+    # 10퍼마다 출력하기
+    data = response[len(headers)+4:]
+    progress = len(data)
+    if (progress/total)*100 > cnt+10:
+      print(f"Current Downloading {progress}/{total} (bytes) {(progress/total)*100:.0f}%",end="\n")
+      cnt+=10
+    elif (progress/total)*100 ==100:
+      print(f"Current Downloading {progress}/{total} (bytes) {(progress/total)*100:.0f}%",end="\n")
+
+  # 200이 아니면 처리
+  if st==1:
     continue
 
+  # headers =  response.split(b'\r\n\r\n')[0]
+  image = response[len(headers)+4:]
+
+  # # 상태코드에 따른 에러처리
+  # sCode = headers.split(b'\r\n')[0].decode()
+  # status = sCode.split()
+  # if (int(status[1]) != 200):
+  #   print(" ".join(status[1:]),"\r\n")
+  #   continue
 
   # 마지막 출력
-  print("Download Complete:",f"{serverName[3:][1]},",f"{sum}/{total}","\r\n")
+  print("Download Complete:",f"{serverName[-1]},",f"{progress}/{total}","\r\n")
 
   # save image
-  f = open('palladio.JPG', 'wb')
+  f = open(serverName[-1], 'wb')
   f.write(image)
   f.close()
-
-# 소켓 연결 종료
+  # 소켓 연결 종료
   s.close()
