36ee5594b6c2b93bed7c88cbdb43e8d052cda1e4
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-05-04 20:26:05 +0900

    컴네 과제 2 끝

--- hw2/HW2_20213047.py
+++ hw2/HW2_20213047.py
@@ -2,11 +2,11 @@ import sys
 import os
 import socket
 
-serverPort = int(sys.argv[1])
+serverPort = int(sys.argv[1]) # 테스트시 입력한 포트로
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('', serverPort))
-
 serverSocket.listen(1)
+
 print("Student ID : 20213047")
 print("Name : Yewon Lee")
 
@@ -25,21 +25,21 @@ Content-Type: text/html
 
 """
 
-def FileConverter(type, data):
+def FileConverter(type, data): # 변환
     print(f"finish {len(data)}, {len(data)}", flush=True)
     return CONTENT_BODY.replace("{type}", type).replace("{length}", str(len(data))).encode('ascii')+data
 
 try:
     while True:
-        clientSocket, addr = serverSocket.accept()
+        clientSocket, addr = serverSocket.accept() # 클라이언트
 
         print(f"Connection : Host IP {addr[0]}, Port {addr[1]}, socket 4", flush=True)
 
-        request = clientSocket.recv(1024).strip()
+        request = clientSocket.recv(1024).strip() # 요청
 
-        headers = request.split(b'\r\n\r\n')[0]
+        headers = request.split(b'\r\n\r\n')[0] # 나눠서 헤더 분리
         header_lines = headers.split(b'\r\n')[1:]  # 요청 라인을 제외하고 헤더만 추출
-        cnt = len(header_lines) 
+        cnt = len(header_lines) # 헤더 라인의 수
 
         for i in headers.split(b'\r\n'):
             if i.startswith(b'GET'):
@@ -54,16 +54,14 @@ try:
             if os.path.exists(filename) and os.path.isfile(filename):
                 with open(filename, 'rb') as f:
                     file_data = f.read()
-                if filename.split('.')[2] == "html":
+                if filename.split('.')[2] == "html": # html
                     clientSocket.sendall(FileConverter('text/html', file_data))
-                elif filename.split('.')[2] == "jpg":
+                elif filename.split('.')[2] == "jpg": # jpg
                     clientSocket.sendall(FileConverter('image/jpeg', file_data))
-                else:
-                    clientSocket.sendall(FileConverter('text/plain', file_data))
             else:
                 print(f"Server Error : No such file {filename}!", flush=True)
                 clientSocket.sendall(ERROR_BODY.encode('ascii'))
 
-        clientSocket.close()
+        clientSocket.close() # 클라이언트 소켓 종료
 finally:
-    serverSocket.close()
+    serverSocket.close() # 서버 소켓 종료
